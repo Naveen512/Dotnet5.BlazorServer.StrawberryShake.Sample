78aa9fbeef2779c1fd19833fd65701e15b5388e6
hchowdary <hchowdary@qualminds.com>
2021-06-26 18:51:54 +0530

    first commit

---
+++ Models/Gadgets.cs
@@ -0,0 +1,11 @@
+namespace Dot5.Bwasm.StraberryShake.Sample.Models
+{
+    public class Gadgets
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Brand { get; set; }
+        public decimal Cost { get; set; }
+        public string Type { get; set; }
+    }
+}
